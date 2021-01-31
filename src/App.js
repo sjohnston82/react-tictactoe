4c4afdf79d5e83b9ca5afce861efc0e55aab2df4
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2021-01-30 17:41:44 -0800

    adding winning styles

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import { calculateWinner } from './helpers';
 
 import "./styles/root.scss";
 
+const NEW_GAME = [
+  {board: Array(9).fill(null), isXNext: true},
+];
+
 const App = () => {
-  const [history, setHistory] = useState([
-    {board: Array(9).fill(null), isXNext: true},
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
   
 
-  const winner = calculateWinner(current.board);
+  const {winner, winningSquares} = calculateWinner(current.board);
   
 
   const handleSquareClick = (position) => {
@@ -43,12 +45,18 @@ const App = () => {
   const moveTo = (move) => {
     setCurrentMove(move);
   }
+
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  }
   
   return (
   <div className="app">
     <h1>TIC TAC TOE</h1>
     <StatusMessage winner={winner} current={current} />
-    <Board board={current.board} handleSquareClick={handleSquareClick} />
+    <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares} />
+    <button type="button" onClick={onNewGame}>Start New Game</button>
     <History history={history} moveTo={moveTo} currentMove={currentMove} />
   </div>
 
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Square from './Square'
 
-const Board = ({ board, handleSquareClick }) => {
+const Board = ({ board, handleSquareClick, winningSquares }) => {
     
    
 
     const renderSquare = (position) => {
-         return <Square value={board[position]} onClick={() => handleSquareClick(position)} />
+        
+        const isWinningSquare = winningSquares.includes(position)
+        
+        return <Square value={board[position]}
+        onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
+        />
     }
     
     return (
